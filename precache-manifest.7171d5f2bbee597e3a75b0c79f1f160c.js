a6926d63443053007c48b0a812f407e8c8816624
nguyễn thiện tâm <nguyentam2112015@gmail.com>
2020-05-09 00:53:28 +0700

    Updates

---
+++ precache-manifest.7171d5f2bbee597e3a75b0c79f1f160c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f441cfdcea0902caff24472b0242732",
+    "url": "/publishweb/index.html"
+  },
+  {
+    "revision": "300a3a5cbef3a2ca140d",
+    "url": "/publishweb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "41bed98b1eed71704b75",
+    "url": "/publishweb/static/js/2.bb3ce595.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/publishweb/static/js/2.bb3ce595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300a3a5cbef3a2ca140d",
+    "url": "/publishweb/static/js/main.65f3fd8d.chunk.js"
+  },
+  {
+    "revision": "c86cc4ad65283174e128",
+    "url": "/publishweb/static/js/runtime-main.c7fd62e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/publishweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
